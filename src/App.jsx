27d76b4c9aa96d4fd9f6df4095c4514378bd11e7
sehyeon <gs1884@naver.com>
2024-05-21 11:18:44 +0900

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BoardWrite } from "./page/board/BoardWrite.jsx";
+import { Home } from "./page/member/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [{ path: "write", element: <BoardWrite /> }],
+  },
+]);
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  );
 }
 
 export default App;
---
+++ src/page/board/BoardWrite.jsx
@@ -0,0 +1,7 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+export function BoardWrite() {
+  return;
+  <Box>글 작성 화면</Box>;
+}
